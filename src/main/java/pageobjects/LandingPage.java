4ae5c20a126dbab30024d3545878c661e5f20bdb
YashuQA <yaseswiniy@duck.com>
2023-02-23 16:24:27 +0530

    Used Page Object Model Design Patter and Page Factory

---
+++ src/main/java/pageobjects/LandingPage.java
@@ -0,0 +1,38 @@
+package pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LandingPage {
+
+	WebDriver driver;
+	
+	//this constructor will receive WebDriver from outside.
+	public LandingPage(WebDriver driver) {
+		
+		this.driver = driver;
+		//"this" means initilize the objects in this class
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//a[@title='My Account']")
+	WebElement myAccountDropDown;
+	
+	//method to return the web element
+	
+	public WebElement myAccountDropDown() {
+		
+		return myAccountDropDown;
+	}
+
+	@FindBy(linkText = "Login")
+	WebElement loginOptions;
+	
+	public WebElement loginOptions() {
+		
+		return loginOptions;
+	}
+	
+}
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,22 +3,52 @@ package tests;
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
+import pageobjects.AccountPage;
+import pageobjects.LandingPage;
+import pageobjects.LoginPage;
 import resources.Base;
 
 public class LoginTest extends Base {
-
+	WebDriver driver;
 //	public LoginTest() {
 //		super();
 //
 //	}
 
 	@Test
-	public void Login() throws IOException {
+	public void Login() throws IOException, InterruptedException {
 
-		WebDriver driver = initilizeDriver();
+		driver = initilizeDriver();
 		driver.get(prop.getProperty("url"));
+		
+		//We need to call the constructor of landing page so we create an object 
+		//and send this class driver 
+		
+		LandingPage landingPage = new LandingPage(driver);
+		
+		landingPage.myAccountDropDown().click(); //this will return the object of myAccount
+		landingPage.loginOptions().click();
 
+		Thread.sleep(3000);
+		
+		LoginPage loginPage = new LoginPage(driver);
+		
+		loginPage.emailAddress().sendKeys(prop.getProperty("email"));
+		loginPage.password().sendKeys(prop.getProperty("password"));
+		loginPage.loginButton().click();
+		
+		AccountPage accountPage = new AccountPage(driver);
+		Assert.assertTrue(accountPage.editAccountInformationLink().isDisplayed());
+		
+	}
+	
+	@AfterMethod
+	public void closure() {
+		
+		driver.close();
 	}
 }
