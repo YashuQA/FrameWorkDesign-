52b2eba7b51bfb6e0df818d748b98ef44cbf0e71
YashuQA <yaseswiniy@duck.com>
2023-02-25 01:08:30 +0530

    Configured log4j for logs

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -4,6 +4,8 @@ package tests;
 
 import java.io.IOException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -18,14 +20,24 @@ import resources.Base;
 public class LoginTest extends Base {
 	WebDriver driver;
 	LoginPage loginPage;
+	Logger log;
 //	public LoginTest() {
 //		super();
 //
 //	}
+	@BeforeMethod
+	public void openApplication() throws IOException {
+		log = LogManager.getLogger(LoginTest.class.getClass());
+		driver = initilizeDriver();
+		log.debug("Browser got launched");
+		driver.get(prop.getProperty("url"));
+		log.debug("Navigated to application URL");
+	}
 
 	@Test()
 	public void Login() throws IOException, InterruptedException {
 
+		
 
 		//We need to call the constructor of landing page so we create an object
 		//and send this class driver
@@ -33,18 +45,23 @@ public class LoginTest extends Base {
 		LandingPage landingPage = new LandingPage(driver);
 
 		landingPage.myAccountDropDown().click(); //this will return the object of myAccount
+		log.debug("Clicked on Account Drop Down");
 		landingPage.loginOptions().click();
-
+		log.debug("Clicked on Login Option");
 		Thread.sleep(3000);
 
 		loginPage = new LoginPage(driver);
 
 		loginPage.emailAddress().sendKeys(prop.getProperty("email"));
+		log.debug("Email Address got entered");
 		loginPage.password().sendKeys(prop.getProperty("password"));
+		log.debug("Password got entered");
 		loginPage.loginButton().click();
+		log.debug("Login button got clicked");
 
 		AccountPage accountPage = new AccountPage(driver);
 		Assert.assertTrue(accountPage.editAccountInformationLink().isDisplayed());
+//		log.info("Assertion is Checked for display");
 
 
 		/*
@@ -56,16 +73,12 @@ public class LoginTest extends Base {
 		 */
 
 	}
-	@BeforeMethod
-	public void openApplication() throws IOException {
 
-		driver = initilizeDriver();
-		driver.get(prop.getProperty("url"));
-	}
 
 	@AfterMethod
 	public void closure() {
 		driver.quit();
+		log.debug("Browser got closed");
 	}
 
 
