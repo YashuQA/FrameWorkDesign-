97238785b2938ff094d0410d36f6755d1bbdf506
YashuQA <yaseswiniy@duck.com>
2023-03-01 15:38:20 +0530

    Listener for success test method

--- src/main/java/listeners/Listeners.java
+++ src/main/java/listeners/Listeners.java
@@ -2,6 +2,7 @@ package listeners;
 
 import java.io.IOException;
 
+
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
@@ -11,23 +12,19 @@ import resources.Base;
 
 public class Listeners extends Base implements ITestListener {
 
+	WebDriver driver = null;
 	
 
 	@Override
 	public void onTestStart(ITestResult result) {
-		// TODO Auto-generated method stub
+
 		
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onTestFailure(ITestResult result) {
-		WebDriver driver=null;
+//		
+//		WebDriver driver=null;
 		//getting the test method name 
 		String testName = result.getName();
 		
@@ -47,6 +44,34 @@ public class Listeners extends Base implements ITestListener {
 		}
 		
 	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		
+		  // WebDriver driver=null; 
+		//getting the test method name 
+		String testName =
+			result.getName();
+
+			try { // we need the driver of the test which is failing. to get that driver
+				// we are writing below line of code
+				driver = (WebDriver) result.getTestClass().getRealClass().getDeclaredField("driver")
+						.get(result.getInstance());
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			} // takeScreenshot method getting from base class 
+			
+			try {
+			takeScreenshot(testName, driver);
+		}catch(IOException e)
+		{
+			 e.printStackTrace();
+			 }
+		}
+		 
+		
+	
 
 	@Override
 	public void onTestSkipped(ITestResult result) {
--- src/test/java/tests/DemoTest.java
+++ src/test/java/tests/DemoTest.java
@@ -1,23 +1,36 @@
 package tests;
 
+
+
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import resources.Base;
 
 public class DemoTest extends Base {
 	public WebDriver driver;
-	@Test
-	public void demo() throws IOException, InterruptedException {
+	
+	@BeforeMethod
+	public void setUp() throws IOException {
 		System.out.println("Demo Test for Listeners");
 		driver = initilizeDriver();
 		driver.get(prop.getProperty("url"));
-		Thread.sleep(3000);
-		Assert.assertTrue(false);
+	}
+	
+	@Test
+	public void demo() throws IOException, InterruptedException {
+		
+	Assert.assertTrue(false);
+	}
+	@Test
+	public void testSuccess() {
+		System.out.println("Test Success");
 	}
 	
 	@AfterMethod
